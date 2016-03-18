487410bd3aef5959f811ff1dc022c53e3f5ae54e
krishna.sh <krishna.sh@BG3WS0035.SSLTUIODC.LOCAL>
2016-03-18 11:00:54 +0530

    c3

--- one/src/one/Two.java
+++ one/src/one/Two.java
@@ -4,7 +4,10 @@ public class Two {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("two two );
+     System.out.println("two two" );
+     System.out.println("two two" );
+     System.out.println("two two" );
+     System.out.println("two two" );
 	}
 
 }
